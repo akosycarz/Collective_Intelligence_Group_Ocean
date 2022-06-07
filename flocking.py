ebdc7807e33039daee0e6516508e6601e2366705
ehkfsjdi <t.k.ahola@student.vu.nl>
2022-06-07 19:25:15 +0100

    partial implementation of flocking behaviour

--- flocking.py
+++ flocking.py
@@ -5,11 +5,67 @@ from vi.config import Config, dataclass, deserialize
 @deserialize
 @dataclass
 class FlockingConfig(Config):
+    alignment_weight: float = 0.6
+    cohesion_weight: float = 0.6
+    separation_weight: float = 0.1
+    delta_time: float = 2
+    mass: int = 30
+    maxVelocity: int = 5
+
+    def weights(self) -> tuple[float , float , float]:
+        return (self.alignment_weight , self.cohesion_weight , self.separation_weight)
     ...
 
 
 class Bird(Agent):
-    ...
+    config: FlockingConfig
+
+    def update(self):
+        pass
+
+    def change_position(self):
+        # Get the neighbours of the boid
+        neighbours = self.in_proximity_accuracy().collect_set()
+        # If the set in empty, wandering behaviour
+        if len(neighbours) == 0:
+            super(Bird, self).change_position()
+        # Else, calculate new direction
+        else:
+            # Calculate alignment
+            alignment = self.alignment(neighbours)
+            # Calculate separation
+            separation = self.separation(neighbours)
+            # Calculate cohesion
+            cohesion = self.cohesion(neighbours)
+            # Calculate new force to update velocity
+            f_total = (self.config.alignment_weight*alignment + self.config.separation_weight*separation + self.config.cohesion_weight*cohesion) / self.config.mass
+            self.move = self.move + f_total
+            # Normalize velocity
+            if self.move[1] > self.config.maxVelocity:
+                self.move.normalize() * self.config.maxVelocity
+            # Change position
+            self.pos = self.pos * self.config.delta_time
+
+    def alignment(self, neighbours):
+        average = [0, 0]
+        for i in neighbours:
+            average += i[0].move
+        
+        return (average / len(neighbours)) - self.move
+    
+    def separation(self, neighbours):
+        distances = [0, 0]
+        for i in neighbours:
+            distances += (self.pos - i[0].pos)
+        return distances*(1/len(neighbours))
+    
+    def cohesion(self, neighbours):
+        positions = [0, 0]
+        for i in neighbours:
+            positions += i[0].pos
+        fc = (positions / len(neighbours)) - self.pos
+
+        return fc - self.move
 
 
 (
@@ -20,3 +76,4 @@ class Bird(Agent):
     # Step 3: Profit! 🎉
     .run()
 )
+
