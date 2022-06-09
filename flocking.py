6dd99611d58d1043ce0165bd9ab77b169c56bbc8
ehkfsjdi <t.k.ahola@student.vu.nl>
2022-06-09 12:22:59 +0100

    Added an obstacle & did tiny changes

--- flocking.py
+++ flocking.py
@@ -1,18 +1,19 @@
 from enum import Enum, auto
+from matplotlib import image
 
 import pygame as pg
 from pygame.math import Vector2
-from vi import Agent, Simulation
+from vi import Agent, Simulation, obstacle
 from vi.config import Config, dataclass, deserialize
 
 
 @deserialize
 @dataclass
 class FlockingConfig(Config):
-    alignment_weight: float = 0.6
-    cohesion_weight: float = 0.6
-    separation_weight: float = 0.1
-    delta_time: float = 2
+    alignment_weight: float = 4.5
+    cohesion_weight: float = 5
+    separation_weight: float = 6
+    delta_time: float = 3
     mass: int = 30
     maxVelocity: int = 5
 
@@ -49,10 +50,10 @@ class Bird(Agent):
             f_total = (self.config.alignment_weight*alignment + self.config.separation_weight*separation + self.config.cohesion_weight*cohesion) / self.config.mass
             self.move = self.move + f_total
             # Normalize velocity
-            if self.move[1] > self.config.maxVelocity:
+            if self.move[1] < self.config.maxVelocity:
                 self.move.normalize() * self.config.maxVelocity
             # Change position
-            self.pos = self.pos * self.config.delta_time
+            self.pos = self.pos + (self.move * self.config.delta_time)
         #END CODE -----------------
 
     def alignment(self, neighbours):
@@ -113,17 +114,21 @@ class FlockingLive(Simulation):
         a, c, s = self.config.weights()
         print(f"A: {a:.1f} - C: {c:.1f} - S: {s:.1f}")
 
-
+config = Config()
+x, y = config.window.as_tuple()
 (
     FlockingLive(
         FlockingConfig(
             image_rotation=True,
             movement_speed=1,
-            radius=50,
+            radius=70,
             seed=1,
         )
     )
     .batch_spawn_agents(50, Bird, images=["images/bird.png"])
+    .spawn_obstacle("images/bubble-full.png", x // 4, y //4)
     .run()
 )
+
+
 
