2e9b7cbbed5df254ad98f861836968dacadb2175
ehkfsjdi <t.k.ahola@student.vu.nl>
2022-06-10 08:57:28 +0100

    Added normalization to  vectors

--- flocking.py
+++ flocking.py
@@ -12,10 +12,11 @@ from vi.config import Config, dataclass, deserialize
 class FlockingConfig(Config):
     alignment_weight: float = 4.5
     cohesion_weight: float = 5
-    separation_weight: float = 6
+    separation_weight: float = 5
     delta_time: float = 3
     mass: int = 30
-    maxVelocity: int = 5
+    maxVelocity: int = 2
+    mode: str = "outside obstacle"
 
     def weights(self) -> tuple[float , float , float]:
         return (self.alignment_weight , self.cohesion_weight , self.separation_weight)
@@ -27,14 +28,13 @@ class Bird(Agent):
 
     def update(self):
         pass
-
         
     def change_position(self):
         # Pac-man-style teleport to the other end of the screen when trying to escape
         self.there_is_no_escape()
         #YOUR CODE HERE -----------
         # Get the neighbours of the boid
-        neighbours = self.in_proximity_accuracy().collect_set()
+        neighbours = list(self.in_proximity_accuracy())
         # If the set in empty, wandering behaviour
         if len(neighbours) == 0:
             super(Bird, self).change_position()
@@ -47,34 +47,37 @@ class Bird(Agent):
             # Calculate cohesion
             cohesion = self.cohesion(neighbours)
             # Calculate new force to update velocity
-            f_total = (self.config.alignment_weight*alignment + self.config.separation_weight*separation + self.config.cohesion_weight*cohesion) / self.config.mass
+            f_total = (self.config.alignment_weight*alignment + self.config.separation_weight*separation + self.config.cohesion_weight*cohesion)/self.config.mass
             self.move = self.move + f_total
             # Normalize velocity
-            if self.move[1] < self.config.maxVelocity:
-                self.move.normalize() * self.config.maxVelocity
+            if Vector2.length(self.move) > self.config.maxVelocity:
+                self.move = self.move.normalize() * self.config.maxVelocity
             # Change position
             self.pos = self.pos + (self.move * self.config.delta_time)
         #END CODE -----------------
 
     def alignment(self, neighbours):
         average = [0, 0]
-        for i in neighbours:
-            average += i[0].move
-        
-        return (average / len(neighbours)) - self.move
+        for i, d in neighbours:
+            average += i.move
+        average /= len(neighbours)
+        alignment = (average.normalize() * self.config.maxVelocity) - self.move
+        return alignment
     
     def separation(self, neighbours):
         distances = [0, 0]
-        for i in neighbours:
-            distances += (self.pos - i[0].pos)
-        return distances*(1/len(neighbours))
+        for i, d in neighbours:
+            distances += (self.pos - i.pos)/d
+        separation = distances / len(neighbours)
+        return separation.normalize() * self.config.maxVelocity
     
     def cohesion(self, neighbours):
         positions = [0, 0]
-        for i in neighbours:
-            positions += i[0].pos
+        for i, d in neighbours:
+            positions += i.pos
         fc = (positions / len(neighbours)) - self.pos
-        return fc - self.move
+        cohesion = fc.normalize() * self.config.maxVelocity
+        return cohesion - self.move
         
 
 class Selection(Enum):
@@ -116,12 +119,13 @@ class FlockingLive(Simulation):
 
 config = Config()
 x, y = config.window.as_tuple()
+
 (
     FlockingLive(
         FlockingConfig(
             image_rotation=True,
             movement_speed=1,
-            radius=70,
+            radius=100,
             seed=1,
         )
     )
