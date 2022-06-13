852b7f97de2c42e3a6dc0398752928241e0ab5b1
ehkfsjdi <t.k.ahola@student.vu.nl>
2022-06-13 18:11:12 +0100

    Aggregation basic implementation with wrong site

--- aggregation.py
+++ aggregation.py
@@ -1,10 +1,11 @@
 from enum import Enum, auto
+from turtle import window_height
 from matplotlib import image
 
 import pygame as pg
 from pygame.math import Vector2
-from sklearn import neighbors
-from vi import Agent, Simulation
+from sklearn import metrics, neighbors
+from vi import Agent, Simulation, util
 from vi.config import Config, dataclass, deserialize
 
 @deserialize
@@ -12,6 +13,9 @@ from vi.config import Config, dataclass, deserialize
 class AggregationConfig(Config):
     # Add all parameters here
     random_walk_time: int = 20
+    constant_a: float = None
+    constant_b: float = None
+    state: str = 'wandering'
 
     def weights(self) -> tuple[float]:
         return (self.random_walk_time)
@@ -31,7 +35,7 @@ class Cockroach(Agent):
         self.pos = Vector2((x, y))
 
     def update(self):
-        # If we detect an aggregation site, we join the aggregation
+        neighbours = self.in_proximity_performance()
         if self.on_site():
             self.freeze_movement()
             
@@ -39,8 +43,32 @@ class Cockroach(Agent):
     """def change_position(self):
         # Pac-man-style teleport to the other end of the screen when trying to escape
         self.there_is_no_escape()"""
+    
+    def join(self, neighbours):
+        # Calculate the joining probability using the number of neighbours
+        probability = None
+        # WHAT FUNCTION HERE
+        # Return True if join the aggregate, else return False
+        if self.on_site() & util.probability(probability):
+            return True
+        else:
+            return False
+
+    def leave(self, neighbours):
+        # Calculate the leaving probability
+        # If there are many neighbours, leave less likely
+        probability = None
+        # WHAT FUNCTION HERE
+        # Return True if leave the aggregate, else return False
+        if util.probability(probability):
+            return True
+        else:
+            return False
 
 config = Config()
+n = 50
+config.window.height = n*(4**2)
+config.window.width = n*(4**2)
 x, y = config.window.as_tuple()
 
 (
@@ -52,7 +80,7 @@ x, y = config.window.as_tuple()
             seed=1,
         )
     )
-    .batch_spawn_agents(50, Cockroach, images=["images/white.png"])
+    .batch_spawn_agents(n, Cockroach, images=["images/white.png"])
     .spawn_site("images/bubble-full.png", x//2, y//2)
     .run()
 )
