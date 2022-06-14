7065424e603025bb22d37d036633479f55604044
ehkfsjdi <t.k.ahola@student.vu.nl>
2022-06-14 09:25:37 +0100

    Aggregation with states and not good probabilities

--- aggregation.py
+++ aggregation.py
@@ -1,24 +1,23 @@
 from enum import Enum, auto
-from turtle import window_height
 from matplotlib import image
+import numpy as np
 
 import pygame as pg
 from pygame.math import Vector2
-from sklearn import metrics, neighbors
 from vi import Agent, Simulation, util
-from vi.config import Config, dataclass, deserialize
+from vi.config import Window, Config, dataclass, deserialize
 
 @deserialize
 @dataclass
 class AggregationConfig(Config):
     # Add all parameters here
-    random_walk_time: int = 20
-    constant_a: float = None
-    constant_b: float = None
-    state: str = 'wandering'
+    D: int = 20
+    joining_prob: float = 0.7
+    leaving_prob: float = 0.01
+    t: int = 10
 
     def weights(self) -> tuple[float]:
-        return (self.random_walk_time)
+        return (self)
     ...
 
 
@@ -26,28 +25,52 @@ class Cockroach(Agent):
     config: AggregationConfig
 
     def on_spawn(self):
-        # Used if we want to override some init parameters
-        # We choose a random starting position that is not inside the site
-        prng = self.shared.prng_move
-        # Change the values
-        x = prng.uniform(286, 750)
-        y = prng.uniform(286, 750)
-        self.pos = Vector2((x, y))
+        # All agents start at the wandering state and with counter 0
+        self.state = 'wandering'
+        self.counter = 0
+        # Create the time constraints max_time =t_join = t_leave
+        # Sample some Gaussian noise
+        noise = np.random.normal()
+        self.max_time = self.config.t + noise
+        # Make sure no agents start at the aggregation site
+        self.pos = self.choose_start_pos()
+        
 
     def update(self):
-        neighbours = self.in_proximity_performance()
-        if self.on_site():
-            self.freeze_movement()
+        # The number of immediate neighbours
+        neighbours = self.in_proximity_performance().count()
+        if self.state == 'wandering': 
+            # If detect an aggregation site, join the aggregation with given 
+            # probability
+            if self.join(neighbours):
+                self.state = 'joining'
+        elif self.state == 'joining':
+            self.counter += 1
+            # When the agent has joined the aggregation, change the state to still
+            if self.counter > self.max_time: 
+                self.freeze_movement()
+                self.state = 'still'
+                self.counter = 0
+        elif self.state == 'still': 
+            self.counter += 1
+            # Leave the aggregate with given probability, but only check 
+            # this every D timesteps
+            if self.counter % self.config.D == 0 and self.leave(neighbours):
+                self.continue_movement()
+                self.counter = 0
+                self.state = 'leaving'
+        elif self.state == 'leaving':
+            self.counter += 1
+            # When the agent has left the aggregation site, change the state to wandering
+            if self.counter > self.max_time: 
+                self.state = 'wandering'
+                self.counter = 0
             
-        
-    """def change_position(self):
-        # Pac-man-style teleport to the other end of the screen when trying to escape
-        self.there_is_no_escape()"""
     
     def join(self, neighbours):
         # Calculate the joining probability using the number of neighbours
-        probability = None
-        # WHAT FUNCTION HERE
+        probability = self.config.joining_prob * neighbours
+        # The probability function is currently random, not based on anything
         # Return True if join the aggregate, else return False
         if self.on_site() & util.probability(probability):
             return True
@@ -56,19 +79,31 @@ class Cockroach(Agent):
 
     def leave(self, neighbours):
         # Calculate the leaving probability
-        # If there are many neighbours, leave less likely
-        probability = None
-        # WHAT FUNCTION HERE
+        # If there are many neighbours, leaving is less likely
+        probability = self.config.leaving_prob * 0.1*neighbours
+        # The probability function is currently random, not based on anything
         # Return True if leave the aggregate, else return False
         if util.probability(probability):
             return True
         else:
             return False
+    
+    def choose_start_pos(self):
+        # Choose a random start position
+        prng = self.shared.prng_move
+        xw, yw = self.config.window.as_tuple()
+        x = prng.uniform(0, xw)
+        y = prng.uniform(0, yw)
+        # If it is inside an aggregation site, repeat the choice
+        if ((xw//2-110) < x < (xw//2+110)) & ((yw//2-110) < y < (yw//2+110)):
+            new_pos = self.choose_start_pos()
+            return new_pos
+        # Else, return the position
+        else:
+            return Vector2((x, y))
 
 config = Config()
 n = 50
-config.window.height = n*(4**2)
-config.window.width = n*(4**2)
 x, y = config.window.as_tuple()
 
 (
@@ -78,6 +113,7 @@ x, y = config.window.as_tuple()
             movement_speed=1,
             radius=100,
             seed=1,
+            window=Window(width=n*(4**2), height=n*(4**2)),
         )
     )
     .batch_spawn_agents(n, Cockroach, images=["images/white.png"])
