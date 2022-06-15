75f96ed5500cb2f5dd45608a4edcb83d7dba55af
Taina <76994910+ehkfsjdi@users.noreply.github.com>
2022-06-15 07:37:25 +0100

    Changed probability functions according to slides

--- aggregation.py
+++ aggregation.py
@@ -1,6 +1,7 @@
 from enum import Enum, auto
 from matplotlib import image
 import numpy as np
+import math
 
 import pygame as pg
 from pygame.math import Vector2
@@ -12,9 +13,12 @@ from vi.config import Window, Config, dataclass, deserialize
 class AggregationConfig(Config):
     # Add all parameters here
     D: int = 20
-    joining_prob: float = 0.5
-    leaving_prob: float = 0.1
-    t: int = 75
+    factor_a: float = 2.6
+    factor_b: float = 2.2
+    t_join: int = 50
+    t_leave: int = 150
+    small_circle_radius: int = 128/2
+    big_circle_radius: int = 200/2
 
     def weights(self) -> tuple[float]:
         return (self)
@@ -31,12 +35,15 @@ class Cockroach(Agent):
         # Create the time constraints max_time =t_join = t_leave
         # Sample some Gaussian noise
         noise = np.random.normal()
-        self.max_time = self.config.t + noise
+        self.max_join_time = self.config.t_join + noise
+        self.max_leave_time = self.config.t_leave
         # Make sure no agents start at the aggregation site
         self.pos = self.choose_start_pos()
         
 
     def update(self):
+        # Save the current state of the agent
+        self.save_data("state", self.state)
         # The number of immediate neighbours
         neighbours = self.in_proximity_performance().count()
         if self.state == 'wandering': 
@@ -47,7 +54,7 @@ class Cockroach(Agent):
         elif self.state == 'joining':
             self.counter += 1
             # When the agent has joined the aggregation, change the state to still
-            if self.counter > self.max_time: 
+            if self.counter > self.max_join_time: 
                 self.freeze_movement()
                 self.state = 'still'
                 self.counter = 0
@@ -62,30 +69,28 @@ class Cockroach(Agent):
         elif self.state == 'leaving':
             self.counter += 1
             # When the agent has left the aggregation site, change the state to wandering
-            if self.counter > self.max_time: 
+            if self.counter > self.max_leave_time: 
                 self.state = 'wandering'
                 self.counter = 0
+
             
-    
     def join(self, neighbours):
         # Calculate the joining probability using the number of neighbours
-        if neighbours != 0:
-            probability = self.config.joining_prob ** (1/neighbours)
-        else: probability = self.config.joining_prob
-        # The probability function is not yet perfect
+        # The probability to stop is 0.03 if no neighbours and at most 0.51
+        probability = 0.03 + 0.48*(1 - math.exp(-self.config.factor_a * neighbours))
         # Return True if join the aggregate, else return False
-        if self.on_site() & util.probability(probability):
+        if self.on_site() and util.probability(probability):
             return True
         else:
             return False
 
+
     def leave(self, neighbours):
         # Calculate the leaving probability
         # If there are many neighbours, leaving is less likely
-        if neighbours != 0:
-            probability = self.config.joining_prob ** neighbours
-        else: probability = self.config.joining_prob
-        # The probability function is not perfect
+        # If there are no neighbours, it is nearly certain that the agents
+        # leave, probability is 1
+        probability = math.exp(-self.config.factor_b * neighbours)
         # Return True if leave the aggregate, else return False
         if util.probability(probability):
             return True
@@ -96,14 +101,16 @@ class Cockroach(Agent):
         # Choose a random start position
         prng = self.shared.prng_move
         xw, yw = self.config.window.as_tuple()
+        r1 = self.config.small_circle_radius
+        r2 = self.config.big_circle_radius
         x = prng.uniform(0, xw)
         y = prng.uniform(0, yw)
         # If it is inside an aggregation site, repeat the choice
-        # One circle: if ((xw//2-75) < x < (xw//2+75)) and ((yw//2-75) < y < (yw//2+75)):
+        # One circle: if ((xw//2-r1) < x < (xw//2+r1)) and ((yw//2-r1) < y < (yw//2+r1)):
         # Two same size circles: 
-        # if (((xw//4-75) < x < (xw//4+75)) or (((xw//4)*3-75) < x < ((xw//4)*3+75))) and ((yw//2-75) < y < (yw//2+75)):
+        # if (((xw//4-r1) < x < (xw//4+r1)) or (((xw//4)*3-r1) < x < ((xw//4)*3+r1))) and ((yw//2-r1) < y < (yw//2+r1)):
         # Two different size circles:
-        if (((xw//4-100) < x < (xw//4+100)) or (((xw//4)*3-75) < x < ((xw//4)*3+75))) and ((yw//2-75) < y < (yw//2+75)):
+        if (((xw//4-r2) < x < (xw//4+r2)) and ((yw//2+r2) < y < (yw//2+r2))) or ((((xw//4)*3-r1) < x < ((xw//4)*3+r1)) and ((yw//2-r1) < y < (yw//2+r1))):
             new_pos = self.choose_start_pos()
             return new_pos
         # Else, return the position
@@ -136,8 +143,6 @@ x, y = config.window.as_tuple()
     .spawn_site("images/circle.png", (x//4)*3, y//2)
     .run()
 )
-
-
 
 
 
