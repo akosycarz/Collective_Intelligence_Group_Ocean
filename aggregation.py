7f0f53fcf86d393c42f22e5a067f4149f8cd6df4
Taina <76994910+ehkfsjdi@users.noreply.github.com>
2022-06-16 15:29:00 +0200

    Added popular agents into cockroach-class
    Added popular agents into cockroach-class & made sure popular cockroaches only go to the left circle. The average popularity not included in ptobability calculations yet.

--- aggregation.py
+++ aggregation.py
@@ -1,21 +1,15 @@
 from enum import Enum, auto
-from matplotlib import image
-import random
+import matplotlib.pyplot as plt
 import numpy as np
 import math
 import polars as pl
 import seaborn as sns
+import random
 
 import pygame as pg
 from pygame.math import Vector2
 from vi import Agent, Simulation, util
 from vi.config import Window, Config, dataclass, deserialize
-
-
-def sigmoid(x):
-    sig = 1 / (1 + math.exp(-x))
-    return sig
-
 
 @deserialize
 @dataclass
@@ -24,15 +18,15 @@ class AggregationConfig(Config):
     D: int = 20
     factor_a: float = 2.6
     factor_b: float = 2.2
-    factor_p: float = 1
     t_join: int = 50
-    t_leave: int = 150
-    small_circle_radius: int = 128 / 2
-    big_circle_radius: int = 200 / 2
+    t_leave: int = 50
+    small_circle_radius: int = 128/2
+    big_circle_radius: int = 200/2
+    number_popular_agents: int = 0
+    max_popular_agents: int = 6
 
     def weights(self) -> tuple[float, float]:
         return (self.factor_a, self.factor_b)
-
     ...
 
 
@@ -43,33 +37,28 @@ class Cockroach(Agent):
         # All agents start at the wandering state and with counter 0
         self.state = 'wandering'
         self.counter = 0
-        self.popularity = random.randint(1, 3)
-        # Create the time constraints max_time =t_join = t_leave
+        if self.config.number_popular_agents <= self.config.max_popular_agents:
+            self.config.number_popular_agents += 1
+            self.popularity = 3
+            self.change_image(1)
+        else:
+            self.popularity = random.randint(1, 2)
+            self.change_image(0)
+        # Create the time constraints
         # Sample some Gaussian noise
         noise = np.random.normal()
         self.max_join_time = self.config.t_join + noise
         self.max_leave_time = self.config.t_leave
         # Make sure no agents start at the aggregation site
         self.pos = self.choose_start_pos()
-
-    def average_popularity(self):
-        neighbours = self.in_proximity_performance()
-        popularity = 0
-
-        for neighbour in neighbours:
-            popularity += neighbour.popularity
-        if neighbours.count() > 0:
-            self.config.factor_p = popularity / neighbours.count()
-        else:
-            self.config.factor_p = 1
-        return self.config.factor_p
+        
 
     def update(self):
         # Save the current state of the agent
         self.save_data("state", self.state)
         # The number of immediate neighbours
-        neighbours = self.in_proximity_performance()
-        if self.state == 'wandering':
+        neighbours = list(self.in_proximity_performance())
+        if self.state == 'wandering': 
             # If detect an aggregation site, join the aggregation with given 
             # probability
             if self.join(neighbours):
@@ -77,11 +66,11 @@ class Cockroach(Agent):
         elif self.state == 'joining':
             self.counter += 1
             # When the agent has joined the aggregation, change the state to still
-            if self.counter > self.max_join_time:
+            if self.counter > self.max_join_time: 
                 self.freeze_movement()
                 self.state = 'still'
                 self.counter = 0
-        elif self.state == 'still':
+        elif self.state == 'still': 
             self.counter += 1
             # Leave the aggregate with given probability, but only check 
             # this every D timesteps
@@ -92,33 +81,42 @@ class Cockroach(Agent):
         elif self.state == 'leaving':
             self.counter += 1
             # When the agent has left the aggregation site, change the state to wandering
-            if self.counter > self.max_leave_time:
+            if self.counter > self.max_leave_time: 
                 self.state = 'wandering'
                 self.counter = 0
 
+            
     def join(self, neighbours):
+        # The average popularity of the neighbours
+        avg_pop = self.neighbour_popularity(neighbours)
         # Calculate the joining probability using the number of neighbours
         # The probability to stop is 0.03 if no neighbours and at most 0.51
-        probability = 0.03 + 0.48 * (1 - math.e ** -(self.config.factor_a * neighbours.count()))
+        probability = 0.03 + 0.48*(1 - math.e**(-self.config.factor_a * len(neighbours)))
         # Return True if join the aggregate, else return False
-        if self.on_site() and util.probability(probability):
-            return True
+        if self.popularity == 3:
+            if self.on_site_id() == 0:
+                return True
         else:
-            return False
+            if self.on_site_id() is not None and util.probability(probability):
+                return True
+        return False
+
 
     def leave(self, neighbours):
+        # The average popularity of the neighbours
+        avg_pop = self.neighbour_popularity(neighbours)
         # Calculate the leaving probability
         # If there are many neighbours, leaving is less likely
         # If there are no neighbours, it is nearly certain that the agents
         # leave, probability is 1
-        probability = math.e ** -(self.config.factor_b * neighbours.count()) * self.average_popularity()
+        probability = math.e**(-self.config.factor_b * len(neighbours))
         print(f'Leave popularity: {probability}')
         # Return True if leave the aggregate, else return False
         if util.probability(probability):
             return True
         else:
             return False
-
+    
     def choose_start_pos(self):
         # Choose a random start position
         prng = self.shared.prng_move
@@ -130,137 +128,34 @@ class Cockroach(Agent):
         # If it is inside an aggregation site, repeat the choice
         # One circle: if ((xw//2-r1) < x < (xw//2+r1)) and ((yw//2-r1) < y < (yw//2+r1)):
         # Two same size circles: 
-        # if (((xw//4-r1) < x < (xw//4+r1)) or (((xw//4)*3-r1) < x < ((xw//4)*3+r1))) and ((yw//2-r1) < y < (yw//2+r1)):
+        if (((xw//4-r1) < x < (xw//4+r1)) or (((xw//4)*3-r1) < x < ((xw//4)*3+r1))) and ((yw//2-r1) < y < (yw//2+r1)):
         # Two different size circles:
-        if (((xw // 4 - r2) < x < (xw // 4 + r2)) and ((yw // 2 + r2) < y < (yw // 2 + r2))) or (
-                (((xw // 4) * 3 - r1) < x < ((xw // 4) * 3 + r1)) and ((yw // 2 - r1) < y < (yw // 2 + r1))):
+        #if (((xw//4-r2) < x < (xw//4+r2)) and ((yw//2+r2) < y < (yw//2+r2))) or ((((xw//4)*3-r1) < x < ((xw//4)*3+r1)) and ((yw//2-r1) < y < (yw//2+r1))):
             new_pos = self.choose_start_pos()
             return new_pos
         # Else, return the position
         else:
             return Vector2((x, y))
-
+    
     def neighbour_popularity(self, neighbours):
         avg_popularity = 0
-        for i in neighbours:
-            avg_popularity += i.popularity
-
-        return avg_popularity / neighbours.count()
-
-class PopularCockroach(Agent):
-    config: AggregationConfig
-
-    def on_spawn(self):
-        # All agents start at the wandering state and with counter 0
-        self.state = 'wandering'
-        self.counter = 0
-        self.popularity = 9
-        # Create the time constraints max_time =t_join = t_leave
-        # Sample some Gaussian noise
-        noise = np.random.normal()
-        self.max_join_time = self.config.t_join + noise
-        self.max_leave_time = self.config.t_leave
-        # Make sure no agents start at the aggregation site
-        self.pos = self.choose_start_pos()
-
-    def average_popularity(self):
-        neighbours = self.in_proximity_performance()
-        popularity = 0
-
-        for neighbour in neighbours:
-            popularity += neighbour.popularity
-        if neighbours.count() > 0:
-            self.config.factor_p = popularity / neighbours.count()
+        if len(neighbours) != 0:
+            for i in neighbours:
+                avg_popularity += i.popularity
+            if avg_popularity / len(neighbours) > 3:
+                return 3
+            elif avg_popularity / len(neighbours) < 1:
+                return 1
+            else:
+                return avg_popularity / len(neighbours)
         else:
-            self.config.factor_p = 1
-        return self.config.factor_p
+            return 1
 
-    def update(self):
-        # Save the current state of the agent
-        self.save_data("state", self.state)
-        # The number of immediate neighbours
-        neighbours = self.in_proximity_performance()
-        if self.state == 'wandering':
-            # If detect an aggregation site, join the aggregation with given
-            # probability
-            if self.on_site():
-                self.state = 'joining'
-        elif self.state == 'joining':
-            self.counter += 1
-            # When the agent has joined the aggregation, change the state to still
-            if self.counter > self.max_join_time:
-                self.freeze_movement()
-                self.state = 'still'
-                self.counter = 0
-        elif self.state == 'still':
-            self.counter += 1
-            # Leave the aggregate with given probability, but only check
-            # this every D timesteps
-        #     if self.counter % self.config.D == 0 and self.leave(neighbours):
-        #         self.continue_movement()
-        #         self.counter = 0
-        #         self.state = 'leaving'
-        elif self.state == 'leaving':
-        #     self.counter += 1
-            # When the agent has left the aggregation site, change the state to wandering
-            if self.counter > self.max_leave_time:
-                self.state = 'wandering'
-                self.counter = 0
-
-    def join(self, neighbours):
-        # Calculate the joining probability using the number of neighbours
-        # The probability to stop is 0.03 if no neighbours and at most 0.51
-        probability = 0.03 + 0.48 * (1 - math.e ** (-self.config.factor_a * neighbours.count()))
-        # Return True if join the aggregate, else return False
-        if self.on_site() and util.probability(probability):
-            return True
-        else:
-            return False
-
-    def leave(self, neighbours):
-        # Calculate the leaving probability
-        # If there are many neighbours, leaving is less likely
-        # If there are no neighbours, it is nearly certain that the agents
-        # leave, probability is 1
-        probability = sigmoid(math.e ** (-self.config.factor_b * neighbours.count()))
-        # Return True if leave the aggregate, else return False
-        if util.probability(probability):
-            return True
-        else:
-            return False
-
-    def choose_start_pos(self):
-        # Choose a random start position
-        prng = self.shared.prng_move
-        xw, yw = self.config.window.as_tuple()
-        r1 = self.config.small_circle_radius
-        r2 = self.config.big_circle_radius
-        x = prng.uniform(0, xw)
-        y = prng.uniform(0, yw)
-        # If it is inside an aggregation site, repeat the choice
-        # One circle: if ((xw//2-r1) < x < (xw//2+r1)) and ((yw//2-r1) < y < (yw//2+r1)):
-        # Two same size circles:
-        # if (((xw//4-r1) < x < (xw//4+r1)) or (((xw//4)*3-r1) < x < ((xw//4)*3+r1))) and ((yw//2-r1) < y < (yw//2+r1)):
-        # Two different size circles:
-        if (((xw // 4 - r2) < x < (xw // 4 + r2)) and ((yw // 2 + r2) < y < (yw // 2 + r2))) or (
-                (((xw // 4) * 3 - r1) < x < ((xw // 4) * 3 + r1)) and ((yw // 2 - r1) < y < (yw // 2 + r1))):
-            new_pos = self.choose_start_pos()
-            return new_pos
-        # Else, return the position
-        else:
-            return Vector2((x, y))
-
-    def neighbour_popularity(self, neighbours):
-        avg_popularity = 0
-        for i in neighbours:
-            avg_popularity += i.popularity
-
-        return avg_popularity / neighbours.count()
 
 config = Config()
 n = 50
-config.window.height = n * (4 ** 2)
-config.window.width = n * (4 ** 2)
+config.window.height = n*(4**2)
+config.window.width = n*(4**2)
 x, y = config.window.as_tuple()
 
 df = (
@@ -270,39 +165,35 @@ df = (
             movement_speed=1,
             radius=100,
             seed=1,
-            window=Window(width=n * (4 ** 2), height=n * (4 ** 2)),
-            duration=1000 * 60,
+            window=Window(width=n*(4**2), height=n*(4**2)),
+            duration=30*60,
             fps_limit=0,
         )
     )
-        .batch_spawn_agents(n, Cockroach, images=["images/white.png"])
-        .batch_spawn_agents(10, PopularCockroach, images=['images/popular_cockroaches.png'])
-
-        # One circle: .spawn_site("images/circle.png", x//2, y//2)
-        # Two same size circles:
-        .spawn_site("images/circle.png", x // 4, y // 2)
-        .spawn_site("images/circle.png", (x // 4) * 3, y // 2)
-        # Two different sized circles:
-        # .spawn_site("images/bigger_circle.png", x//4, y//2)
-        # .spawn_site("images/circle.png", (x//4)*3, y//2)
-        .run()
-        .snapshots
-        # Get the number of stopped agents per timeframe and also per aggregation
-        # site
-        .filter(pl.col("state") == 'still')
-        .with_columns([
-        ((((x // 4) * 3 + 64) > pl.col("x")) & (pl.col("x") > ((x // 4) * 3 - 64)) & ((y // 2 + 64) > pl.col("y")) & (
-                    pl.col("y") > (y // 2 - 64))).alias("small aggregate"),
-        (((x // 4 + 100) > pl.col("x")) & (pl.col("x") > (x // 4 - 100)) & ((y // 2 + 100) > pl.col("y")) & (
-                    pl.col("y") > (y // 2 - 100))).alias("big aggregate")
+    .batch_spawn_agents(n, Cockroach, images=["images/white.png", "images/red.png"])
+    # One circle: .spawn_site("images/circle.png", x//2, y//2)
+    # Two same size circles: 
+    .spawn_site("images/circle.png", x//4, y//2)
+    .spawn_site("images/circle.png", (x//4)*3, y//2)
+    # Two different sizde circles:
+    #.spawn_site("images/bigger_circle.png", x//4, y//2)
+    #.spawn_site("images/circle.png", (x//4)*3, y//2)
+    .run()
+    .snapshots
+    # Get the number of stopped agents per timeframe and also per aggregation
+    # site
+    .filter(pl.col("state") == 'still')
+    .with_columns([
+        ((((x//4)*3+64) > pl.col("x")) & (pl.col("x") > ((x//4)*3-64)) & ((y//2+64) > pl.col("y")) & (pl.col("y") > (y//2-64))).alias("small aggregate"),
+        (((x//4+100) > pl.col("x")) & (pl.col("x") > (x//4-100)) & ((y//2+100) > pl.col("y")) & (pl.col("y") > (y//2-100))).alias("big aggregate")
     ])
-        .groupby(["frame"])
-        .agg([
+    .groupby(["frame"])
+    .agg([
         pl.count('id').alias("number of stopped agents"),
         pl.col("small aggregate").cumsum().alias("small aggregate size").last(),
         pl.col("big aggregate").cumsum().alias("big aggregate size").last()
     ])
-        .sort(["frame", "number of stopped agents"])
+    .sort(["frame", "number of stopped agents"])
 )
 
 print(df)
@@ -312,9 +203,9 @@ print('Proportion of agents in big aggregate: {}'.format(df.get_column("big aggr
 # Plot the number of stopped agents per frame
 plot1 = sns.relplot(x=df["frame"], y=df["number of stopped agents"], kind="line")
 plot1.savefig("stopped.png", dpi=300)
-# Plot the small aggregate size per frame
 plot2 = sns.relplot(x=df["frame"], y=df["small aggregate size"], kind="line")
 plot2.savefig("small_aggregate.png", dpi=300)
-# Plot the big aggregate size per frame
 plot3 = sns.relplot(x=df["frame"], y=df["big aggregate size"], kind="line")
 plot3.savefig("big_aggregate.png", dpi=300)
+
+
