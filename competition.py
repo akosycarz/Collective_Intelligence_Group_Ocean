41b4995bfb90250a8fd1121d9217ae29228f5a97
ehkfsjdi <t.k.ahola@student.vu.nl>
2022-06-21 17:53:33 +0200

    Fixed bugs with gender & reproduction

--- competition.py
+++ competition.py
@@ -12,13 +12,14 @@ import random
 @dataclass
 class CompetitionConfig(Config):
     # Add all parameters here
-    rabbit_reproducton_prob: float = 0.01
+    rabbit_reproduction_prob: float = 0.9
     fox_reproduction_prob: float = 0.1
     energy_decrease_rate: float = 0.99
-    energy_decrease_rate_rabbit: float = 0.99
-    energy_decrease_rate_fox: float = 0.5
+    energy_decrease_rate_rabbit: float = 0.01
+    energy_decrease_rate_fox: float = 0.01
     grass_grow_rate: int = 200
     aging_rate: float = 0.001
+    offspring_number: int = 3
 
     def weights(self) -> tuple[float]:
         return (self)
@@ -45,9 +46,9 @@ class Fox(Agent):
         # Increase the age of the animal
         self.age += self.config.aging_rate
         # Decrease the energy of the fox
-        self.energy *= self.config.energy_decrease_rate_fox
+        self.energy -= (self.config.energy_decrease_rate_fox*self.age)
         # If the fox has no energy, it dies
-        if self.energy == 0:
+        if self.energy <= 0:
             self.death_cause = "starvation"
             self.kill()
         # Check if there are rabbits near
@@ -81,8 +82,9 @@ class Fox(Agent):
                        .first()
                   )
         if partner is not None and util.probability(self.config.fox_reproduction_prob):
-            # Currently only gets one offspring, should it be more?
-            self.reproduce()
+            if self.gender == ["female"] and partner.gender == ["male"]:
+                # Currently only gets one offspring, should it be more?
+                self.reproduce()
             # Reproduction takes energy, so decrease energy
             self.energy /= 2
 
@@ -107,9 +109,9 @@ class Rabbit(Agent):
         # Increase the age of the animal
         self.age += self.config.aging_rate
         # Decrease the energy of the rabbit
-        self.energy *= self.config.energy_decrease_rate_rabbit
+        self.energy -= (self.config.energy_decrease_rate_fox*self.age)
         # If the rabbit has no energy, it dies
-        if self.energy == 0:
+        if self.energy <= 0:
             self.death_cause = "starvation"
             self.kill() 
         # Check if there is grass near
@@ -144,12 +146,11 @@ class Rabbit(Agent):
                        .filter(lambda agent: agent.age > 0.3)
                        .first()
                   )
-        if partner is not None and util.probability(self.config.fox_reproduction_prob) and self.energy >= 0.5:
+        if partner is not None and util.probability(self.config.rabbit_reproduction_prob) and self.energy >= 0.5:
             # A female can only be cloned and it must meet a male to do it
-            if self.gender == "female" and partner.gender == 'male':
+            if self.gender == ["female"] and partner.gender == ['male']:
                 # Currently only gets 3 offspring, should it be more?
-                babies_count = 3
-                for n in range(0, babies_count):
+                for n in range(0, self.config.offspring_number):
                     self.reproduce()
                 # Reproduction takes energy, so decrease energy
                 self.energy /= 2
@@ -182,7 +183,7 @@ class Grass(Agent):
             
             
 config = Config()
-n_fox = 40
+n_fox = 10
 n_rabbit = 40
 n_grass = 50
 n = n_fox + n_rabbit
@@ -225,13 +226,7 @@ plot1.savefig("number_agents.png", dpi=300)
 
 
 
-"""Age 
--    rabbits (live expectancy 8-12 years)
--    foxes (live expectancy 1-9 years)"""
-# Should we have a unique time to die or would it be better if we have that
-# the energy is decreased more at every frame the older the agent is?
 # Should the grass be in specific places?
 # Is it in multiple places?
-# Should the animals have more than one offspring each reproduction time?
 
 
